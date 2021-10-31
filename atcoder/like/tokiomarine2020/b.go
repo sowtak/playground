f143b8447818462385010b30e8c6d2cb566db945
tkyngs <zovaakasowa@gmail.com>
2021-11-01 07:57:47 +0900

    .

---
+++ atcoder/like/tokiomarine2020/b.go
@@ -0,0 +1,40 @@
+
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+  "fmt"
+)
+
+var sc, wr = bufio.NewScanner(os.Stdin), bufio.NewWriter(os.Stdout)
+
+func ns() string { sc.Scan(); return sc.Text()}
+func nr() []rune { return []rune(ns()) }
+func ni() int { a, _ := strconv.Atoi(ns()); return a}
+func ni64() int64 { a, _ := strconv.ParseInt(ns(), 10, 64); return a}
+func nf() float64 { a, _ := strconv.ParseFloat(ns(), 64); return a}
+func nis(n int) []int {a := make([]int, n);for i:=0;i<n;i++ {a[i] = ni()};return a}
+
+func main() {
+	defer wr.Flush()
+	sc.Split(bufio.ScanWords)
+	sc.Buffer(make([]byte, 1001), 1001001)
+ 
+  a,v,b,w,t:=ni(),ni(),ni(),ni(),ni()
+  if b>a {
+    if a+v*t >= b+w*t {
+      fmt.Println("YES")
+    } else {
+      fmt.Println("NO")
+    }
+  } else {
+    if a-v*t <= b-w*t {
+      fmt.Println("YES")
+    } else {
+      fmt.Println("NO")
+    }
+  }
+
+}
