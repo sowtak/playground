291ccdc7d045a256acf415b40188b1dbe30bed21
Sowa Takayanagi <sowtak.swe@gmail.com>
2022-12-29 21:03:53 +0900

    update

---
+++ ac/r/144/c.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+#include <set>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <cmath>
+#include <cstring>
+#define ll long long
+#define rep(idx,b,e) for(int idx=(b);idx<e;idx++)
+#define rrep(idx,b,e) for(int idx=b;idx>e;idx--)
+template <typename T> inline bool chmin(T &a, T &b) {if (a>b){a=b; return true;} else return false;}
+template <typename T> inline bool chmax(T &a, T &b) {if (a<b){a=b; return true;} else return false;}
+using namespace std;
+
+const int MOD = 1000000007;
+const int MOD2 = 998244353;
+const int MAX = 2147483647;
+int dx4[4]={1,0,-1,0};
+int dy4[4]={0,1,0,-1};
+int dx8[8]={0,1,0,-1,1,1,-1,-1};
+int dy8[8]={1,0,-1,0,1,-1,1,-1};
+
+
+int N,K;
+int main() {
+  cin>>N>>K;
+  v
+
+}
